bc3be3c69db97e05afce0f599a65bbf3e47e5a6d
Rachmizard <rachmizard11072000@gmail.com>
2019-10-09 13:24:53 +0700

    first commit and complete all code

---
+++ server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser');
+  port = process.env.PORT || 3000;
+
+
+const mysql = require('mysql');
+// connection configurations
+const mc = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'mydb'
+});
+ 
+// connect to database
+mc.connect();
+
+app.listen(port);
+
+console.log('API server started on: ' + port);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./src/routes/appRoutes'); //importing route
+routes(app); //register the route
